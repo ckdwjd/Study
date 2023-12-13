ff5b0f7bacfa58844acbd7a33e7ec5eb8b3e02de
hodubrother <ckdwjd2717@naver.com>
2023-12-13 18:32:18 +0900

    Spring Introduction

--- Spring_Workspace/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Spring_Workspace/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,2 +1,16 @@
-package hello.hellospring.controller;public class HelloController {
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello"; // templates에 존재하는 hello.html로 이동(html파일의 경로)
+    }
+
 }
