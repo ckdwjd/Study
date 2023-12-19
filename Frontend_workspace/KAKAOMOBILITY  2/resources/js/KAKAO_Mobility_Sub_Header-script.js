9fd1b6edb275fda90b9e18c6f9748a7129a24e05
Oh Chang Jeong <ckdwjd2717@gmail.com>
2023-12-20 06:49:09 +0900

    Frontend Workspace

--- Frontend_workspace/KAKAOMOBILITY  2/resources/js/KAKAO_Mobility_Sub_Header-script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $(".header-menuList dt").hover(function(){
-        $(this).find("dd").stop(true,true).slideDown();
-    }, function(){
-        $(this).find("dd").stop(true,true).slideUp();
-    })
-});
